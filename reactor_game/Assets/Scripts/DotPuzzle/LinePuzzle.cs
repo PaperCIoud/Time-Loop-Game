08a8420baaa6d302a3612e3679227abd43dea856
PaperCIoud <haoni@mit.edu>
2021-04-28 21:06:50 -0400

    Made prefabs for all interactable

--- reactor_game/Assets/Scripts/DotPuzzle/LinePuzzle.cs
+++ reactor_game/Assets/Scripts/DotPuzzle/LinePuzzle.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 public class LinePuzzle : MonoBehaviour
 {
 
-    private List<string> answer = new List<string> { "0-1", "1-2", "2-3" };
+    private List<string> answer = new List<string> { "0-2","0-6","1-6","1-3","2-6","3-6","4-5","4-6","5-6" };
     private List<string> userAnswer = new List<string> { };
     private bool completed = false;
 
